d626c9f822849eb3da1e2cdf00e1560c1d070da5
devjsf <imjoshuasf@gmail.com>
2026-03-10 15:14:24 -0700

    fix: update Stripe API version

--- scheduleme/lib/stripe.ts
+++ scheduleme/lib/stripe.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 if (!process.env.STRIPE_SECRET_KEY) throw new Error('Missing STRIPE_SECRET_KEY');
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-02-24.acacia',
+  apiVersion: '2026-02-25.clover',
 });
 
 export default stripe;
