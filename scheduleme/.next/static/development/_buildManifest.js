fb5fa57336a054ec1aa94d1dcc9f2ea20d25382f
devjsf <imjoshuasf@gmail.com>
2026-03-11 13:55:02 -0700

    chore: untrack .next from git

--- scheduleme/.next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/business":["static\u002Fchunks\u002Fpages\u002Fbusiness.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fbusiness"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> parent of 3cac9c5 (fix: use BusinessNav in dashboard)
