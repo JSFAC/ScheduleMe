09306b72811bf77b777048d5517e8185a546f590
devjsf <imjoshuasf@gmail.com>
2026-03-15 17:49:38 -0700

    feat: add apple-touch-icon link tag

--- scheduleme/pages/_app.tsx
+++ scheduleme/pages/_app.tsx
@@ -69,6 +69,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="ScheduleMe" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="mobile-web-app-capable" content="yes" />
       </Head>
       {showOverlay && (
