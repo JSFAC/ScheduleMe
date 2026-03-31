7b2b2a4e35c47f86d2a8d8658d2018d57e7ffe70
devjsf <imjoshuasf@gmail.com>
2026-03-31 11:00:47 -0700

    make emails send for new business application

---
+++ ios/xcode/ScheduleMe/ScheduleMe/ScheduleMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleMeApp.swift
+//  ScheduleMe
+//
+//  Created by Joshua on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
