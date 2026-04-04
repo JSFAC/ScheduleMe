f55db560f3d44c0a2dba311da1f45439a9b6e7bd
devjsf <imjoshuasf@gmail.com>
2026-04-04 14:10:10 -0700

    Tighten biz hero spacing

--- scheduleme/pages/biz/[slug].tsx
+++ scheduleme/pages/biz/[slug].tsx
@@ -781,15 +781,15 @@ export default function BizPage() {
             )}
           </div>
         )}
-        <div className="relative" style={{ height: 470, background: bg }}>
-          <div className="relative h-full w-full mx-auto flex items-center justify-center" style={{ maxWidth: 980, paddingTop: 26, paddingBottom: 0 }}>
+        <div className="relative" style={{ height: 410, background: bg }}>
+          <div className="relative h-full w-full mx-auto flex items-center justify-center" style={{ maxWidth: 980, paddingTop: 18, paddingBottom: 0 }}>
             <div className="relative flex items-center justify-center w-full" style={{ maxWidth: 800 }}>
               {imgs[galleryIdx] && (
                 <img
                   src={imgs[galleryIdx]}
                   alt={biz.name}
                   className="object-contain rounded-2xl"
-                  style={{ maxHeight: 320, maxWidth: '100%' }}
+                  style={{ maxHeight: 290, maxWidth: '100%' }}
                   onClick={() => imgs.length > 0 && setGalleryOpen(true)}
                 />
               )}
