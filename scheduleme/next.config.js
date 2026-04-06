2155eeff2ada607166b08799a462a0bb64d6ddd3
devjsf <imjoshuasf@gmail.com>
2026-04-06 09:20:29 -0700

    Allow hCaptcha styles in CSP

--- scheduleme/next.config.js
+++ scheduleme/next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const ContentSecurityPolicy = [
   "default-src 'self'",
   `script-src 'self' 'unsafe-inline' ${isProd ? '' : "'unsafe-eval'"} https://js.stripe.com https://hcaptcha.com https://*.hcaptcha.com`,
-  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com",
   "img-src 'self' data: blob: https: https://hcaptcha.com https://*.hcaptcha.com",
   "font-src 'self' data: https://fonts.gstatic.com",
   "connect-src 'self' https: wss: https://api.stripe.com https://ipapi.co https://*.supabase.co https://*.supabase.in https://hcaptcha.com https://*.hcaptcha.com",
